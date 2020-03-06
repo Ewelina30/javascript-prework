0486f54a2bc79674d24b2bde04731760275d5a5c
Ewelina Słyk <ewelinaslyk@wp.pl>
2020-03-06 23:50:16 +0100

    Add reading computer movement

--- js/script.js
+++ js/script.js
@@ -14,11 +14,11 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
 
-if(randomNumber == 1){
+if(randomNumber === 1){
   computerMove = 'kamień';
-} else if (randomNumber == 2) {
-	computerMove = 'papier';
-} else if (randomNumber == 3) {
+} else if (randomNumber === 2) {
+	 computerMove = 'papier';
+} else if (randomNumber === 3){
 	computerMove = 'nożyce';
 }
 
