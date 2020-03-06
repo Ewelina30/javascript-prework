77f742c270c7cbe373eeeb01c3ba7bc850e5da95
Ewelina Słyk <ewelinaslyk@wp.pl>
2020-03-07 00:13:17 +0100

    Add game score

--- js/script.js
+++ js/script.js
@@ -14,11 +14,11 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
 
-if(randomNumber === 1){
+if(randomNumber == 1){
   computerMove = 'kamień';
-} else if (randomNumber === 2) {
+} else if (randomNumber == 2) {
 	 computerMove = 'papier';
-} else if (randomNumber === 3){
+} else if (randomNumber == 3){
 	computerMove = 'nożyce';
 }
 
@@ -30,13 +30,45 @@ console.log('Gracz wpisał: ' + playerInput);
 
 let playerMove = 'nieznany ruch';
 
-if(playerInput === '1'){
+if(playerInput == '1'){
   playerMove = 'kamień';
-} else if (playerInput === '2') {
+} else if (playerInput == '2') {
 	playerMove = 'papier';
-} else if (playerInput === '3'){
+} else if (playerInput == '3'){
 	playerMove = 'nożyce';
 }
 
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}
+
+if( computerMove == 'kamień' && playerMove == 'nożyce'){
+  printMessage('Ty przegrywasz!');
+}
+if( computerMove == 'kamień' && playerMove == 'kamień'){
+  printMessage('Remis!');
+}
+if( computerMove == 'papier' && playerMove == 'papier'){
+  printMessage('Remis!');
+}
+if( computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+}
+if( computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Ty przerywasz!');
+}
+if( computerMove == 'nożyce' && playerMove == 'papier'){
+  printMessage('Ty przegrywasz!');
+}
+if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+  printMessage('Remis');
+}
+if( computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+}
+if(playerMove == 'nieznany ruch'){
+    printMessage('Spróbuj jeszcze raz')
+}
